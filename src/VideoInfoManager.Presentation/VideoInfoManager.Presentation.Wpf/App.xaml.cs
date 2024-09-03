20be918930dbdf970c9f08e232487fc992d51972
Jose Antonio Guerrero Lopez <lordmad4000@hotmail.com>
2024-09-03 16:01:21 +0200

    Added Wpf presentation project, references to other projects and configure DI.

---
+++ src/VideoInfoManager.Presentation/VideoInfoManager.Presentation.Wpf/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VideoInfoManager.Presentation.Wpf.Configuration;
+
+namespace VideoInfoManager.Presentation.Wpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : System.Windows.Application
+{
+    [STAThread]
+    public static void Main()
+    {
+        var app = new VideoInfoManager.Presentation.Wpf.App();
+
+        var services = new ServiceCollection();
+        ConfigureServices.AddDbContext(services);
+        ConfigureServices.AddConfiguration(services);
+        ConfigureServices.AddServices(services);
+
+        using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+        {
+            ConfigureServices.DataBaseEnsureCreated(serviceProvider);
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            app.Run(mainWindow);
+        }
+    }
+
+    //protected override void OnStartup(StartupEventArgs e)
+    //{
+    //    base.OnStartup(e);
+    //    Console.WriteLine("PEPE");
+    //    // here you take control
+    //}
+}
