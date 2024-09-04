0449fee2e37618d358d8ed20c8c9a4a309029ba2
Jose Antonio Guerrero Lopez <lordmad4000@hotmail.com>
2024-09-04 10:58:52 +0200

    Added Icon to MainWindow.

--- src/VideoInfoManager.Presentation/VideoInfoManager.Presentation.Wpf/MainWindow.xaml.cs
+++ src/VideoInfoManager.Presentation/VideoInfoManager.Presentation.Wpf/MainWindow.xaml.cs
@@ -4,10 +4,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VideoInfoManager.Presentation.Wpf.Helpers;
 
 namespace VideoInfoManager.Presentation.Wpf
 {
@@ -19,6 +21,17 @@ namespace VideoInfoManager.Presentation.Wpf
         public MainWindow()
         {
             InitializeComponent();
+            InitializeWindow();
         }
+
+        private void InitializeWindow()
+        {
+            this.Height = 500;
+            this.Width = 560;
+            var icon = IconExtractor.Extract("shell32.dll", 218, true);
+            ImageSource imageSource = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            this.Icon = imageSource;
+        }
+
     }
 }
